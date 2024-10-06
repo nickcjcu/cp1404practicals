cdb6995f9fbc1d03e2c60431df1e28d45ecc24c4
Nikodem <nikodem@ciolek.my.jcu.edu.au>
2024-10-06 23:19:15 +1000

    added task file

---
+++ prac_03/randoms.py
@@ -0,0 +1,37 @@
+'''
+
+QUESTION ANSWERS:
+
+What did you see on line 1?
+
+    the number 10
+
+What was the smallest number you could have seen, what was the largest?
+
+    smallest could be 5 and largest could be 20
+
+What did you see on line 2?
+
+    the number 3
+
+What was the smallest number you could have seen, what was the largest?
+
+    smallest could be 3 and larger could be 9
+
+Could line 2 have produced a 4?
+
+    no, as the results can only be increments by 2 starting at 3
+
+What did you see on line 3?
+
+    3.015007813833377
+
+What was the smallest number you could have seen, what was the largest?
+
+    the smallest could be 2.5 and larger could be 5.5 
+
+'''
+
+import random
+
+print(random.uniform(1, 100))
